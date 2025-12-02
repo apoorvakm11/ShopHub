87fdf94d8d207bbf6aec2c9ff49ebeec20f7aade
apoorvakm11 <apoorvakm2004@gmail.com>
2025-12-02 19:45:10 +0530

    Delete .next/dev directory

--- .next/dev/static/chunks/app_page_tsx_3be48321._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5a25c4d4._.js",
-  "static/chunks/_852422b9._.js"
-],
-    source: "dynamic"
-});
